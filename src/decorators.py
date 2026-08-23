f93060e393a86bffa11792d1de106ed0242b9bc8
Ivan Glushenkov <glushman966@gmail.com>
2026-08-23 23:32:22 +0300

    update decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -10,16 +10,20 @@ log_file = log_folder / "mylog.txt"
 
 
 def logger_init() -> logging.Logger:
-    logger = logging.getLogger("app")
-    logger.setLevel(logging.INFO)
+    logg = logging.getLogger("app")
+    logg.setLevel(logging.INFO)
 
-    if not logger.handlers:
+    if not logg.handlers:
         handler = logging.FileHandler(log_file, mode="w", encoding="utf-8")
         formatter = logging.Formatter("%(asctime)s - %(funcName)s - %(name)s - %(levelname)s: - %(message)s")
+        consol = logging.StreamHandler()
+        # consol.setFormatter(formatter)
         handler.setFormatter(formatter)
-        logger.addHandler(handler)
 
-    return logger
+        logg.addHandler(consol)
+        logg.addHandler(handler)
+
+    return logg
 
 
 logger = logger_init()
@@ -33,11 +37,11 @@ def log() -> Callable:
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
-                logger.info(f"Успешный запуск функции: {func.__name__} - {func.__doc__}")
+                logger.info("Успешный запуск функции: %s - %s", {func.__name__}, {func.__doc__})
                 return result
 
             except Exception as e:
-                logger.exception(f"Ошибка: {func.__name__}: {type(e).__name__}: {e}")
+                logger.exception("Ошибка: %s: %s: %s", {func.__name__}, {type(e).__name__}, {e})
                 raise
 
         return wrapper
