1254942a801d5188e001c2c5633891048d7a1b1b
Ivan Glushenkov <glushman966@gmail.com>
2026-08-23 23:32:56 +0300

    update services.py

--- src/services.py
+++ src/services.py
@@ -1,10 +1,6 @@
-"""
-Сервисы
-"""
-
 import json
 import re
-from typing import Dict
+from typing import Any
 
 import pandas as pd
 from pandas import DataFrame
@@ -15,16 +11,18 @@ from src.decorators import logger
 
 def normalize_phone(value: str) -> str:
     """Оставляет только цифры в номере телефона."""
+
     return re.sub(r"\D", "", value)
 
 
 @log()
-def find_transactions_by_phone(df: DataFrame, phone: str) -> list:
+def find_transactions_by_phone(df: DataFrame, phone: str) -> list[Any]:
     """Возвращает JSON со всеми транзакциями, содержащими указанный номер телефона."""
 
     normalized_phone = normalize_phone(phone)
     if not normalized_phone:
         logger.warning("Ошибка ввода номера телефона: тел.=%s", phone)
+
         return []
 
     normalized_search = df["Описание"].astype(str).str.replace(r"\D", "", regex=True)
@@ -33,6 +31,7 @@ def find_transactions_by_phone(df: DataFrame, phone: str) -> list:
 
     if not mask.any():
         logger.warning("Введенный номер телефона не найден: тел.=%s", phone)
+
         return []
 
     result_df = df.loc[mask, ["Дата операции", "Сумма платежа", "Категория", "Описание"]]
@@ -42,7 +41,7 @@ def find_transactions_by_phone(df: DataFrame, phone: str) -> list:
 
 
 @log()
-def cashback_analysis(df: DataFrame, year: int, month: int) -> Dict[str, float]:
+def cashback_analysis(df: DataFrame, year: int, month: int) -> str:
     """Анализирует категории повышенного кэшбека."""
 
     df = df.copy()
@@ -58,7 +57,8 @@ def cashback_analysis(df: DataFrame, year: int, month: int) -> Dict[str, float]:
 
     if filter_data.empty:
         logger.warning("Кэшбек за данный период не найден: год=%s, месяц=%s", year, month)
-        return "По вашему зпросу ничего не найдено."
+
+        return "По вашему запросу ничего не найдено."
 
     expenses_by_category = filter_data.groupby("Категория")["Сумма платежа"].sum()
 
